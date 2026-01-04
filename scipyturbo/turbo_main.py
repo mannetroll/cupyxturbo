1b18fbd738c82251209583fc98741f75b8be9bc3
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-04 19:58:03 +0100

    N=34560

--- scipyturbo/turbo_main.py
+++ scipyturbo/turbo_main.py
@@ -430,7 +430,7 @@ class MainWindow(QMainWindow):
         # Steps selector
         self.steps_combo = QComboBox()
         self.steps_combo.setToolTip("S: Max steps before reset/stop")
-        self.steps_combo.addItems(["1000", "2000", "5000", "10000", "25000", "50000", "1E5", "2E5", "3E5", "1E6", "1E7"])
+        self.steps_combo.addItems(["100", "1000", "2000", "5000", "10000", "25000", "50000", "1E5", "2E5", "3E5", "1E6", "1E7"])
         self.steps_combo.setCurrentText("10000")
 
         # Update selector
