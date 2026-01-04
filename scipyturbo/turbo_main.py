246f1720c3512374f7474cd3265e779a772ff737
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-04 21:18:34 +0100

    CFL=0.85

--- scipyturbo/turbo_main.py
+++ scipyturbo/turbo_main.py
@@ -427,7 +427,7 @@ class MainWindow(QMainWindow):
         # CFL selector
         self.cfl_combo = QComboBox()
         self.cfl_combo.setToolTip("L: Controlling Δt (CFL)")
-        self.cfl_combo.addItems(["0.05", "0.1", "0.15", "0.25", "0.5", "0.75", "0.95"])
+        self.cfl_combo.addItems(["0.05", "0.1", "0.15", "0.25", "0.5", "0.75", "0.85", "0.95"])
         self.cfl_combo.setCurrentText(str(self.sim.cfl))
 
         # Steps selector
