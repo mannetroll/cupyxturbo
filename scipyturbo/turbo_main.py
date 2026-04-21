48dd34fc7127dfcf9a47067ce91eee63bdc72a8f
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-04-21 22:38:30 +0200

    steps_combo

--- scipyturbo/turbo_main.py
+++ scipyturbo/turbo_main.py
@@ -446,13 +446,13 @@ class MainWindow(QMainWindow):
         self.steps_combo = QComboBox()
         self.steps_combo.setToolTip("S: Max steps before reset/stop")
         self.steps_combo.addItems(["100", "1000", "2000", "5000", "10000", "25000", "50000", "1E5", "2E5", "3E5", "1E6", "1E7"])
-        self.steps_combo.setCurrentText("50000")
+        self.steps_combo.setCurrentText(steps)
 
         # Update selector
         self.update_combo = QComboBox()
         self.update_combo.setToolTip("U: Update intervall")
         self.update_combo.addItems(["2", "5", "10", "20", "50", "100", "1E3"])
-        self.update_combo.setCurrentText("20")
+        self.update_combo.setCurrentText(update)
 
         self.auto_reset_checkbox = QCheckBox()
         self.auto_reset_checkbox.setToolTip("If checked, simulation auto-resets")
